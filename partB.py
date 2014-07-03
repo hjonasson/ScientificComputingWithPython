9dd4ff074a5f210ab92617bc429158e92168e5e1
Hjörtur Jónasson <hjortur.jonasson@kaust.edu.sa>
2014-07-03 13:48:34 +1000

    Finished half of this exercise on counting within files

---
+++ partB.py
@@ -0,0 +1,25 @@
+#Part B, Strings, Lists, Tuples, Loops, Conditionals, File I/O
+
+#Count lines and words in the Gettysburg address
+def getGA(file):
+	
+	file = open(file,'r')
+	wordcount = 0
+	for word in file.read().split():
+		wordcount += 1
+
+	for i,l in enumerate(file):
+		pass
+
+	print 'The number of words in the Gettysburg address is '+str(wordcount)+' and the number of lines is '+str(i+1)
+
+#Count vowels in the Gettysburg address
+def vowelCount(file):
+	
+	file = open(file)
+	counts = {i:0 for i in 'aeiouAEIOU'}
+	for i in file.read():
+		if i in counts:
+			counts[i] += 1
+	
+	print 'The vowel count in the Gettysburg address is:', counts
