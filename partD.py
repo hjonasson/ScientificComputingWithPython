9c32772902b65e44fa8c9e5efd3b554743631d9b
Hjörtur Jónasson <hjortur.jonasson@kaust.edu.sa>
2014-07-07 11:52:23 +1000

    May be the last one, the rest relies a lot on lecture notes I dont have

--- partD.py
+++ partD.py
@@ -1,5 +1,6 @@
 import partC
 import numpy as np
+import matplotlib.pyplot as plt
 
 def matrixExp(file,newFile):
 
@@ -8,3 +9,10 @@ def matrixExp(file,newFile):
 	l = np.diag(np.exp(l))
 	expA = np.dot(np.dot(v,l),np.dot(v,np.eye(3,3)))
 	partC.matrixWrite(expA,newfile)
+
+def plotMaxSep(file):
+
+	file = open(file).readlines()
+	dmax = [float(line) for line in file]
+	plt.plot(dmax)
+	plt.show
